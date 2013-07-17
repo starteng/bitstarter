a99a87e8ccb5c8faddac9559e6ca6dd94af21332
UPA <upa@ubuntu.(none)>
2013-07-16 20:07:55 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(responseString);
+  response.send('responseString');
 });
 
 var port = process.env.PORT || 5000;
